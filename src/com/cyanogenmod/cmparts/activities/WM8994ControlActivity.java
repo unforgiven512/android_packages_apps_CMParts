9ce339b85072a1ea3906cebb7d11cd88408c823b
Gerad Munsch <gerad.munsch@gmail.com>
2011-10-08 04:05:56 -0400

    Added even more options!
    Change-Id: I6de865a27e98c35105d76fcc22a735ca39581c11

--- src/com/cyanogenmod/cmparts/activities/WM8994ControlActivity.java
+++ src/com/cyanogenmod/cmparts/activities/WM8994ControlActivity.java
@@ -42,21 +42,38 @@ public class WM8994ControlActivity extends PreferenceActivity /*implements
     public static final String WM8994_ENABLE = "/sys/devices/virtual/misc/voodoo_sound_control/enable";
     public static final String WM8994_SPEAKER_TUNING = "/sys/devices/virtual/misc/voodoo_sound/speaker_tuning";
     public static final String WM8994_MONO_DOWNMIX = "/sys/devices/virtual/misc/voodoo_sound/mono_downmix";
+    public static final String WM8994_STEREO_EXPANSION = "/sys/devices/virtual/misc/voodoo_sound/stereo_expansion";
+    public static final String WM8994_DAC_DIRECT = "/sys/devices/virtual/misc/voodoo_sound/dac_direct";
+    public static final String WM8994_DAC_OSR128 = "/sys/devices/virtual/misc/voodoo_sound/dac_osr128";
+    public static final String WM8994_ADC_OSR128 = "/sys/devices/virtual/misc/voodoo_sound/adc_osr128";
+    public static final String WM8994_FLL_TUNING = "/sys/devices/virtual/misc/voodoo_sound/fll_tuning";
     
     // Preference Objects
     private CheckBoxPreference mWm8994EnablePref;
     private CheckBoxPreference mSpeakerTuning;
     private CheckBoxPreference mMonoDownmix;
+    private CheckBoxPreference mStereoExpansion;
+    private CheckBoxPreference mDacDirect;
+    private CheckBoxPreference mDacOsr128;
+    private CheckBoxPreference mAdcOsr128;
+    private CheckBoxPreference mFllTuning;
     
     // XML item names
     private static final String WM8994_ENABLE_PREF = "pref_wm8994_control_enable";
     private static final String SPEAKER_TUNING_PREF = "pref_wm8994_speaker_tuning";
     private static final String MONO_DOWNMIX_PREF = "pref_wm8994_mono_downmix";
+    private static final String STEREO_EXPANSION_PREF = "pref_wm8994_stereo_expansion";
+    private static final String DAC_DIRECT_PREF = "pref_wm8994_dac_direct";
+    private static final String DAC_OSR128_PREF = "pref_wm8994_dac_osr128";
+    private static final String ADC_OSR128_PREF = "pref_wm8994_adc_osr128";
+    private static final String FLL_TUNING_PREF = "pref_wm8994_fll_tuning";
     
     // Categories
     private static final String GENERAL_CATEGORY = "general_category";
     private static final String INTERNAL_SPEAKER_CATEGORY = "wm8994_internal_speaker_category";
     private static final String SIGNAL_PROCESSING_CATEGORY = "wm8994_signal_processing_category";
+    private static final String DAC_CONFIG_CATEGORY = "wm8994_dac_config_category";
+    private static final String CODEC_OPTIMIZATION_CATEGORY = "wm8994_codec_optimization_category";
     
     // Misc
     private static final String PREF_ENABLED = "1";
@@ -97,6 +114,37 @@ public class WM8994ControlActivity extends PreferenceActivity /*implements
         mMonoDownmix = (CheckBoxPreference) prefSet.findPreference(MONO_DOWNMIX_PREF);
         mMonoDownmix.setChecked(PREF_ENABLED.equals(temp));
         
+        // stereo expansion
+        temp = readOneLine(WM8994_STEREO_EXPANSION);
+        mStereoExpansion = (CheckBoxPreference) prefSet.findPreference(STEREO_EXPANSION_PREF);
+        mStereoExpansion.setChecked(PREF_ENABLED.equals(temp));
+        
+        // D/AC A/DC category
+        PreferenceCategory dacConfigCategory = (PreferenceCategory)prefSet.findPreference(DAC_CONFIG_CATEGORY);
+        
+        // DAC direct
+        temp = readOneLine(WM8994_DAC_DIRECT);
+        mDacDirect = (CheckBoxPreference) prefSet.findPreference(DAC_DIRECT_PREF);
+        mDacDirect.setChecked(PREF_ENABLED.equals(temp));
+        
+        // DAC OSR128
+        temp = readOneLine(WM8994_DAC_OSR128);
+        mDacOsr128 = (CheckBoxPreference) prefSet.findPreference(DAC_OSR128_PREF);
+        mDacOsr128.setChecked(PREF_ENABLED.equals(temp));
+        
+        // ADC OSR128
+        temp = readOneLine(WM8994_ADC_OSR128);
+        mAdcOsr128 = (CheckBoxPreference) prefSet.findPreference(ADC_OSR128_PREF);
+        mAdcOsr128.setChecked(PREF_ENABLED.equals(temp));
+        
+        // CODEC optimization category
+        PreferenceCategory codecOptimizationCategory = (PreferenceCategory)prefSet.findPreference(CODEC_OPTIMIZATION_CATEGORY);
+        
+        // FLL tuning
+        temp = readOneLine(WM8994_FLL_TUNING);
+        mFllTuning = (CheckBoxPreference) prefSet.findPreference(FLL_TUNING_PREF);
+        mFllTuning.setChecked(PREF_ENABLED.equals(temp));
+        
     }
     
     // Preference change action for check boxes
@@ -119,6 +167,36 @@ public class WM8994ControlActivity extends PreferenceActivity /*implements
     	if (preference == mMonoDownmix) {
     		String boxValue = mMonoDownmix.isChecked() ? "1" : "0";
     		commitSysfsPreference(WM8994_MONO_DOWNMIX, boxValue);
+    	}
+    	
+    	// Stereo expansion enable/disable box
+    	if (preference == mStereoExpansion) {
+    		String boxValue = mStereoExpansion.isChecked() ? "1" : "0";
+    		commitSysfsPreference(WM8994_STEREO_EXPANSION, boxValue);
+    	}
+    	
+    	// D/AC direct
+    	if (preference == mDacDirect) {
+    		String boxValue = mDacDirect.isChecked() ? "1" : "0";
+    		commitSysfsPreference(WM8994_DAC_DIRECT, boxValue);
+    	}
+    	
+    	// D/AC OSR128
+    	if (preference == mDacOsr128) {
+    		String boxValue = mDacOsr128.isChecked() ? "1" : "0";
+    		commitSysfsPreference(WM8994_DAC_OSR128, boxValue);
+    	}
+    	
+    	// A/DC OSR128
+    	if (preference == mAdcOsr128) {
+    		String boxValue = mAdcOsr128.isChecked() ? "1" : "0";
+    		commitSysfsPreference(WM8994_ADC_OSR128, boxValue);
+    	}
+    	
+    	// FLL tuning
+    	if (preference == mFllTuning) {
+    		String boxValue = mFllTuning.isChecked() ? "1" : "0";
+    		commitSysfsPreference(WM8994_FLL_TUNING, boxValue);
     	}
     	
     	// otherwise quit, and return false (if preference name doesn't match a condition)
